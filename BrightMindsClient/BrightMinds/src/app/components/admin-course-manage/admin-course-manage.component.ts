fd425d451d364c8ab9089e8c868a5364b6a5d867
Drwildy <drwildeoffical@gmail.com>
2020-05-07 23:55:21 -0500

    Final commit

---
+++ BrightMindsClient/BrightMinds/src/app/components/admin-course-manage/admin-course-manage.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { CoursesService } from '../../services/courses.service';
+import { Course } from '../../models/course';
+
+@Component({
+  selector: 'app-admin-course-manage',
+  templateUrl: './admin-course-manage.component.html',
+  styleUrls: ['./admin-course-manage.component.css']
+})
+export class AdminCourseManageComponent implements OnInit {
+
+  approve(course:Course, nStatus:number){
+    alert('this is the course id : '+course.id+" en the new status is : "+nStatus);
+    this.coursesService.manageCourse(course, nStatus).subscribe(
+      result=>{
+        console.log("result is",result);  
+        this.refreshList();      
+      //this.coursesList=result
+    }, 
+      error=>{ console.log(error)}
+    );
+  }
+
+  coursesList:Course[];
+  constructor(private coursesService: CoursesService) { }
+
+  ngOnInit(): void {
+    this.refreshList();
+  }
+
+  refreshList(){
+    this.coursesService.getCourses().subscribe(
+      result=>{
+        console.log("result is",result);
+
+      this.coursesList=result
+    })
+  }
+}
